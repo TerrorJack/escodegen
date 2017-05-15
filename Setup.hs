be6ee7f75d8b113919809fb79a99718076dee3d2
Shao Cheng <astrohavoc@gmail.com>
2017-05-15 14:28:51 +0800

    Make npm install work on Windows.

--- Setup.hs
+++ Setup.hs
@@ -7,16 +7,15 @@ import Distribution.PackageDescription
 import Distribution.Simple
 import Distribution.Simple.InstallDirs hiding (absoluteInstallDirs)
 import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Program
-import Distribution.Verbosity
+import System.Exit
 import System.FilePath
+import System.Process
 
 main :: IO ()
 main =
     defaultMainWithHooks
         simpleUserHooks
-        { hookedPrograms = npm : hookedPrograms simpleUserHooks
-        , postConf =
+        { postConf =
               \args flags pkg_descr lbi -> do
                   encodeFile
                       biPath
@@ -37,11 +36,13 @@ main =
                   let nodedir =
                           datadir (absoluteInstallDirs pkg_descr lbi NoCopyDest) </>
                           "escodegen-server"
-                  runDbProgram
-                      verbose
-                      npm
-                      (withPrograms lbi)
-                      ["install", "--prefix", nodedir]
+                  withCreateProcess ((shell "npm install") {cwd = Just nodedir}) $ \_ _ _ h -> do
+                      r <- waitForProcess h
+                      case r of
+                          ExitSuccess -> pure ()
+                          ExitFailure x ->
+                              fail $
+                              "npm install failed with exit code " ++ show x
                   postCopy simpleUserHooks args copy_flags pkg_descr lbi
         }
   where
@@ -49,4 +50,3 @@ main =
     preAny _ _ = do
         bi <- decodeFile biPath
         pure (Just bi, [])
-    npm = simpleProgram "npm"
