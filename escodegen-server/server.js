f53d0c9ca318f798b8831ae3535af6562fc9be68
Shao Cheng <astrohavoc@gmail.com>
2017-05-15 09:48:41 +0800

    Implement the api server in node.js

--- escodegen-server/server.js
+++ escodegen-server/server.js
@@ -1,3 +1,26 @@
+"use strict";
+
 const esprima = require("esprima");
 const escodegen = require("escodegen");
-const http = require("http");
+
+const app = require("express")();
+
+app.use(require("body-parser").json());
+
+app.post("/esprima", (req, res) => {
+    try {
+        res.json(esprima.parse(req.body.input, req.body.config));
+    } catch (err) {
+        res.json({error: err});
+    }
+});
+
+app.post("/escodegen", (req, res) => {
+    try {
+        res.json(escodegen.generate(req.body.input));
+    } catch (err) {
+        res.json({error: err});
+    }
+});
+
+app.listen(require('minimist')(process.argv.slice(2)).port);
